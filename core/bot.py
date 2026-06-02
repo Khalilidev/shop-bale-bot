826b5caec08d130caf869be2c6c950768dae7c82
Ali Asghar <imaliasgharkhalili@gmail.com>
2026-06-02 23:59:32 +0330

    feat: add messages files

--- core/bot.py
+++ core/bot.py
@@ -2,14 +2,13 @@ from bale import Bot, Message
 from configs import TOKEN
 from core.logging import is_seller
 from keyboards.main_menu_seller import *
+from texts.seller_texts import *
 bot = Bot(TOKEN)
 @bot.event
 async def on_message(message:Message):
     print(f"Message from {message.chat.id}")
     if message.text == '/start':
         if is_seller(message.chat.id):
-            await message.reply("👋 **سلام فروشنده عزیز**\n\nبه پنل مدیریت خوش آمدید. از طریق منوی زیر می‌توانید فروشگاه خود را مدیریت کنید:",
-                                components=main_menu_seller())
-
+            await message.reply(WELLCOME_SELLER_TXT, components=main_menu_seller())
 if __name__ == "__main__":
     bot.run()
