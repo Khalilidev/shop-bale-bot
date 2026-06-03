da395fa618517f9c31f59200f79c2ba797f4cd27
Ali Asghar <imaliasgharkhalili@gmail.com>
2026-06-03 14:00:07 +0330

    fix: fixed name if callback

--- keyboards/seller/main_menu_seller.py
+++ keyboards/seller/main_menu_seller.py
@@ -3,7 +3,7 @@ from callbacks.cb_main_menu_seller import *
 def main_menu_seller():
     keyboard = InlineKeyboardMarkup()
 
-    btn1 = InlineKeyboardButton("دفتر حساب", callback_data=CB_S_ACCOUNT_BOOK)
+    btn1 = InlineKeyboardButton("دفتر حساب", callback_data=CB_SELLER_ACCOUNT_BOOK)
 
     keyboard.add(btn1, row=1)
     
