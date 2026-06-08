5d63d12a3cb7116233fa4d7f007a6f4734ab280c
Ali Asghar <imaliasgharkhalili@gmail.com>
2026-06-08 14:28:09 +0330

    feat: add products table to Database

--- databases/init.py
+++ databases/init.py
@@ -28,5 +28,18 @@ def init_db():
                     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
                     FOREIGN KEY (customer_id) REFERENCES customers(id) ON DELETE CASCADE);""")
     
+    # جدول سوم: محصولات
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS products(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        brand TEXT NOT NULL,
+        description TEXT,
+        price INTEGER NOT NULL,
+        stock_quantity INTEGER NOT NULL DEFAULT 0,
+        path_image TEXT
+    )
+    """)
+    
     conn.commit()
     conn.close()
